8edbe050ff4963489a41b0f848e32c6e94eb9a36
JuliaHarkins <32091645+JuliaHarkins@users.noreply.github.com>
2018-10-18 11:19:18 +0100

    First commit
    basic menu and an object, no current functionality

---
+++ AutoMaticTextClassification/FileReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMaticTextClassification
+{
+    class FileReader
+    {
+        string _BayesingNetworkFolder = "BayesingNetwork";
+        string _TestDataFolder = "TestData";
+
+        FileReader()
+        {
+            if (!System.IO.Directory.Exists(_BayesingNetworkFolder))
+            {
+                System.IO.Directory.CreateDirectory(_BayesingNetworkFolder);
+            }
+            if (!System.IO.Directory.Exists(_TestDataFolder))
+            {
+                System.IO.Directory.CreateDirectory(_TestDataFolder);
+            }
+        }
+        string GetTestData()
+        {
+            return "b";
+        }
+        string LoadSavedNetwork()
+        {
+            return "c";
+        }
+    }
+}
---
+++ AutoMaticTextClassification/Objects/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMaticTextClassification
+{
+    class Category
+    {
+        string _name;
+        Dictionary<string, int> _wordAndCount;
+
+        void RemoveLemmatizingWords()
+        {
+
+        }
+        void AddText(string text)
+        {
+
+        }
+        int GetTotalWords()
+        {
+
+            return 0;
+        }
+
+    }
+}
